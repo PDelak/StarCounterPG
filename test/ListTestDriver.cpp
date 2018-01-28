f88ba458235101a2312887ae57df727d4a93b512
pdelak <przemyslaw.delewski@gmail.com>
2018-01-28 13:49:49 +0100

    initial switch to gtest

--- test/ListTestDriver.cpp
+++ test/ListTestDriver.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <cassert>
 #include "list_tools.h"
 #include "reverse_impl.h"
 
 #include "gtest/gtest.h"
 
-void test_find()
+TEST(ListTest, dummy) {}
+
+TEST(ListTest, test_find)
 {
     List list = makeList();
 
@@ -22,12 +23,12 @@ void test_find()
         return str == L"bolek";
     });
     std::wcout << castNode<Ident>(*it)->name << std::endl;
-    assert(list_length(&list) == 3);
+    EXPECT_EQ(list_length(&list) , 3);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_erase1()
+TEST(ListTest, test_erase1)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
@@ -43,13 +44,13 @@ void test_erase1()
         return str == L"bolek";
     });
     erase<Ident>(it);
-    assert(list_length(&list) == 2);
+    EXPECT_EQ(list_length(&list) , 2);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
 
-void test_erase2()
+TEST(ListTest, test_erase2)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
@@ -65,13 +66,13 @@ void test_erase2()
         return str == L"bolek";
     });
     erase<Ident>(it);
-    assert(list_length(&list) == 1);
+    EXPECT_EQ(list_length(&list) , 1);
     std::wcout << castNode<Ident>(*b)->name << std::endl;
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_erase3()
+TEST(ListTest, test_erase3)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
@@ -87,26 +88,26 @@ void test_erase3()
     BasicListIterator itSecond = it;
     ++itSecond;
     erase<Ident>(it);
-    assert(list_length(&list) == 1);
+    EXPECT_EQ(list_length(&list) , 1);
     std::wcout << castNode<Ident>(*itSecond)->name << std::endl;
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_erase4()
+TEST(ListTest, test_erase4)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
 
 
     auto b = begin(list);
-    assert(list_length(&list) == 1);
+    EXPECT_EQ(list_length(&list) , 1);
     erase<Ident>(b);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
 
-void test_iterate()
+TEST(ListTest, test_iterate)
 {
     List list = makeList();
 
@@ -117,18 +118,17 @@ void test_iterate()
     push_back(list, makeIdent(L"milosz"));
 
     auto it_begin = begin(list);
-    auto it = begin(list);
-
+    
     while (it_begin != end(list)) {
         printIdent(castNode<Ident>(*it_begin));
         ++it_begin;
     }
     std::cout << list_length(&list) << std::endl;
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse1()
+TEST(ListTest, test_reverse1)
 {
     List list = makeList();    
     push_back(list, makeIdent(L"delak"));
@@ -140,10 +140,10 @@ void test_reverse1()
     auto result = reverse_impl_1(list);
     std::wcout << result.c_str() << std::endl;
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse2()
+TEST(ListTest, test_reverse2)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
@@ -155,10 +155,10 @@ void test_reverse2()
     auto result = reverse_impl_2(list);
     std::wcout << result.c_str() << std::endl;
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse3()
+TEST(ListTest, test_reverse3)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
@@ -168,46 +168,46 @@ void test_reverse3()
     push_back(list, makeIdent(L"milosz"));
     auto result = reverse_impl_3(list);
     std::wcout << result.c_str() << std::endl;
-    assert(list_length(&list) == 5);
+    EXPECT_EQ(list_length(&list) , 5);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse4()
+TEST(ListTest, test_reverse4)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
     auto result = reverse_impl_3(list);
     std::wcout << result.c_str() << std::endl;
-    assert(list_length(&list) == 1);
+    EXPECT_EQ(list_length(&list) , 1);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse5()
+TEST(ListTest, test_reverse5)
 {
     List list = makeList();
     auto result = reverse_impl_3(list);
     std::wcout << result.c_str() << std::endl;
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
-void test_reverse6()
+TEST(ListTest, test_reverse6)
 {
     List list = makeList();
     push_back(list, makeIdent(L"delak"));
     push_back(list, makeIdent(L"bolek"));
     auto result = reverse_impl_3(list);
     std::wcout << result.c_str() << std::endl;
-    assert(list_length(&list) == 2);
+    EXPECT_EQ(list_length(&list) , 2);
     clean(list);
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 }
 
 
-void test_ListHolder()
+TEST(ListTest, test_ListHolder)
 {
     List list = makeList();
     {
@@ -218,23 +218,13 @@ void test_ListHolder()
         push_back(list, makeIdent(L"monika"));
         push_back(list, makeIdent(L"milosz"));
     }
-    assert(list_length(&list) == 0);
+    EXPECT_EQ(list_length(&list) , 0);
 
 }
 
-int main() 
-{
-    test_find();
-    test_erase1();
-    test_erase2();
-    test_erase3();
-    test_erase4();
-    test_reverse1();
-    test_reverse2();
-    test_reverse3();
-    test_reverse4();
-    test_reverse5();
-    test_reverse6();
-    test_ListHolder();
-    return 0;
+int main(int argc, char* argv[]) 
+{    
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    
 }
