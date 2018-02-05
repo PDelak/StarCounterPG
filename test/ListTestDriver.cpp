1a2a891d22bee87d770059a0d479ffe76e34fd7a
pdelak <przemyslaw.delewski@gmail.com>
2018-02-05 19:34:32 +0100

    api documentation, cleanup, additional reverse implementation

--- test/ListTestDriver.cpp
+++ test/ListTestDriver.cpp
@@ -1,3 +1,13 @@
+//
+// This file contains tests regarding
+// reverse implementations as well as generic List handling
+// To work with List structure in more convenient way
+// new API was provided that allows to use it in similar way as
+// other STL containers. For instance, List can be used with STL algorithms
+// There are possible further improvements, for instance, building a container similar to STL containers on top 
+// of provided API
+//
+
 #include <functional>
 #include "list_tools.h"
 #include "std_list_trait.h"
@@ -5,6 +15,7 @@
 
 #include "gtest/gtest.h"
 
+// Converts vector of wstrings to List of Idents*
 List buildList(const std::vector<std::wstring>& arg)
 {
     List list = makeList();
@@ -14,6 +25,7 @@ List buildList(const std::vector<std::wstring>& arg)
     return list;
 }
 
+// Converts vector of wstrings to std::list of Node*
 std::list<Node*> buildStdList(const std::vector<std::wstring>& arg)
 {
     std::list<Node*> stdlist;
@@ -23,6 +35,7 @@ std::list<Node*> buildStdList(const std::vector<std::wstring>& arg)
     return stdlist;
 }
 
+// Contains input and result set for reverse implementations
 std::vector<std::pair<std::vector<std::wstring>, std::wstring>> reverseInputOutput = {
     {{ L"" }, L"" },
     {{ L"delak" }, L"delak" },
@@ -30,6 +43,16 @@ std::vector<std::pair<std::vector<std::wstring>, std::wstring>> reverseInputOutp
     {{ L"delak", L"bolek", L"patryk", L"monika", L"milosz" }, L"milosz.monika.patryk.bolek.delak" }
 };
 
+void cleanNodes(const List& list)
+{
+    std::for_each(begin(list), end(list), [&](const ListCell* cell) {
+        delete castNode<Node>(cell);
+    });
+}
+
+// Helper method that takes function as parameter
+// and goes through  whole input set checking result
+// works on List container
 template<typename T>
 void reverseTestHelper(const T& fun)
 {
@@ -39,11 +62,13 @@ void reverseTestHelper(const T& fun)
         const auto& expectedResult = rio.second;
         auto result = fun(list);
         EXPECT_EQ(result, expectedResult);
+        cleanNodes(list);
         clean(list);
         EXPECT_EQ(list_length(&list), 0);
     }
 }
 
+// The same as above but works for std::list
 template<typename T>
 void reverseStdListTestHelper(const T& fun)
 {
@@ -57,7 +82,8 @@ void reverseStdListTestHelper(const T& fun)
     }
 }
 
-
+// Helper function that takes two List(s) and 
+// checks them against equality
 void CheckEQList(const List& list1, const List& list2)
 {
     EXPECT_EQ(list_length(&list1), list_length(&list2));
@@ -74,6 +100,28 @@ void CheckEQList(const List& list1, const List& list2)
     });    
 }
 
+// Reverse test against all reverse implementations
+// works on List type
+TEST(ListTest, test_reverse)
+{
+    reverseTestHelper([](const List& list) { return reverse_impl_1(list);});
+    reverseTestHelper([](const List& list) { return reverse_impl_2(list);});
+    reverseTestHelper([](const List& list) { return reverse_impl_3(list);});
+    reverseTestHelper([](List& list) { return reverse_impl_4(list);});
+
+}
+
+// Reverse test against all reverse implementations
+// works on std::list type
+TEST(ListTest, test_reverse_std_list)
+{
+    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_1(list);});
+    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_2(list);});
+    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_3(list);});
+    reverseStdListTestHelper([](std::list<Node*>& stdlist) { return reverse_impl_4(stdlist);});
+}
+
+// Test std::find_if with List container
 TEST(ListTest, test_find)
 {
     List list = buildList({L"delak", L"bolek", L"patryk"});
@@ -88,10 +136,12 @@ TEST(ListTest, test_find)
     });
     EXPECT_EQ(castNode<Ident>(*it)->name, std::wstring(L"bolek"));
     EXPECT_EQ(list_length(&list) , 3);
+    cleanNodes(list);
     clean(list);
     EXPECT_EQ(list_length(&list) , 0);
 }
 
+// Tests removing middle element from List container
 TEST(ListTest, test_erase1)
 {
     List list = buildList({ L"delak", L"bolek", L"patryk" });
@@ -105,15 +155,18 @@ TEST(ListTest, test_erase1)
         std::wstring str(buff);
         return str == L"bolek";
     });
-    erase<Ident>(it);
+    delete castNode<Node>(*it);
+    erase(it);
     EXPECT_EQ(list_length(&list) , 2);
     CheckEQList(list, resultList);
+    cleanNodes(list);
     clean(list);
+    cleanNodes(resultList);
     clean(resultList);
     EXPECT_EQ(list_length(&list) , 0);
 }
 
-
+// Tests removing last element from 2-elements List container
 TEST(ListTest, test_erase2)
 {
     List list = buildList({ L"delak", L"bolek" });
@@ -126,14 +179,18 @@ TEST(ListTest, test_erase2)
         std::wstring str(buff);
         return str == L"bolek";
     });
-    erase<Ident>(it);
+    delete castNode<Node>(*it);
+    erase(it);
     EXPECT_EQ(list_length(&list) , 1);
     CheckEQList(list, resultList);
+    cleanNodes(list);
     clean(list);
+    cleanNodes(resultList);
     clean(resultList);
     EXPECT_EQ(list_length(&list) , 0);
 }
 
+// Tests removing first element from 2-elements List container
 TEST(ListTest, test_erase3)
 {
     List list = buildList({ L"delak", L"bolek" });
@@ -146,64 +203,77 @@ TEST(ListTest, test_erase3)
     });
     BasicListIterator itSecond = it;
     ++itSecond;
-    erase<Ident>(it);
+    delete castNode<Node>(*it);
+    erase(it);
     EXPECT_EQ(list_length(&list) , 1);
     CheckEQList(list, resultList);
+    cleanNodes(list);
+    cleanNodes(resultList);
     clean(list);
     clean(resultList);
     EXPECT_EQ(list_length(&list) , 0);
 }
 
+// Tests removing element from 1-element List
 TEST(ListTest, test_erase4)
 {
     List list = buildList({ L"delak" });
 
     auto b = begin(list);
     EXPECT_EQ(list_length(&list) , 1);
-    erase<Ident>(b);
+    delete castNode<Node>(*b);
+    erase(b);
     EXPECT_EQ(list_length(&list) , 0);
 }
 
-
-TEST(ListTest, test_iterate)
-{
-    List list = buildList({ L"delak", L"bolek", L"patryk", L"monika", L"milosz" });
-
-    auto it_begin = begin(list);
-    
-    while (it_begin != end(list)) {
-        ++it_begin;
-    }
-    EXPECT_EQ(list_length(&list), 5);
-    clean(list);
-    EXPECT_EQ(list_length(&list) , 0);
-}
-
-
-
+// Tests ListHolder against releasing resources
 TEST(ListTest, test_ListHolder)
 {
     List list = buildList({ L"delak", L"bolek", L"patryk", L"monika", L"milosz" });
     {
         ListHolder listHolder(list);
+        cleanNodes(list);
     }
+
     EXPECT_EQ(list_length(&list) , 0);
 
 }
 
-TEST(ListTest, test_reverse)
+TEST(ListTest, test_copy_node)
 {
-    reverseTestHelper([](const List& list) { return reverse_impl_1(list);});
-    reverseTestHelper([](const List& list) { return reverse_impl_2(list);});
-    reverseTestHelper([](const List& list) { return reverse_impl_3(list);});    
+    List list = buildList({ L"delak", L"bolek", L"patryk", L"monika", L"milosz" });
+    auto cloneF = [](const ListCell* cell) {
+        return makeIdent(castNode<Ident>(cell)->name);
+    };
+    auto listCopy = copy(list, cloneF);
+    CheckEQList(list, listCopy);
+    cleanNodes(list);
+    cleanNodes(listCopy);
+    clean(list);
+    clean(listCopy);
 }
 
-TEST(ListTest, test_reverse_std_list)
+TEST(ListTest, test_copy_int)
 {
-    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_1(list);});
-    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_2(list);});
-    reverseStdListTestHelper([](const std::list<Node*>& list) { return reverse_impl_3(list);}); 
+    List list = makeList();
+    push_back(list, 1);
+    push_back(list, 2);
+    push_back(list, 3);
+    auto cloneF = [](const ListCell* cell) {
+        return cell->data.int_value;
+    };
+    auto listCopy = copy(list, cloneF);
+    auto it = begin(listCopy);
+    EXPECT_EQ(list_length(&list), list_length(&listCopy));
+
+    std::for_each(begin(list), end(list), [&](const ListCell* cell) {
+        EXPECT_EQ(cell->data.int_value, (*it)->data.int_value);
+        ++it;
+    });
+    clean(list);
+    clean(listCopy);
 }
+
 
 int main(int argc, char* argv[]) 
 {    
